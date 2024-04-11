5835a3ec4cceb27abf46099c5cbbfa4cffedcb94
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-04-11 23:26:05 +0000

    Utils: Modifico crear_logger y creo iniciar_config

--- utils/src/utils/hello.c
+++ utils/src/utils/hello.c
@@ -7,20 +7,28 @@ void decir_hola(char *quien)
 
 t_log *crear_logger(char *modulo) // Podria ampliarse o cambiarse
 {
-
-    char *name_logger = string_duplicate(modulo);
+    char *nombre_logger = string_duplicate(modulo);
     char *log = ".log";
-    string_append_with_format(&name_logger, "%s", log);
+    string_append_with_format(&nombre_logger, "%s", log);
 
-    t_log *new_logger;
-    new_logger = log_create(name_logger, modulo, 1, LOG_LEVEL_INFO);
+    t_log *nuevo_logger = log_create(nombre_logger, modulo, 1, LOG_LEVEL_INFO);
 
-    if (new_logger == NULL)
+    if (nuevo_logger == NULL)
     {
         printf("No se pudo iniciar un logger\n");
-
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    return new_logger;
+    return nuevo_logger;
+}
+
+t_config *iniciar_config(t_log logger, char *ruta_archivo)
+{
+    t_config *nuevo_config = config_create(ruta_archivo);
+    if (nuevo_config == NULL)
+    {
+        log_error(logger, "No se pudo crear la configuracion en %s", ruta_archivo);
+        exit(EXIT_FAILURE);
+    }
+    return nuevo_config;
 }
