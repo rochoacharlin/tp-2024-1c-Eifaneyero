22931da0cf6674818f6e5e5944baecf6f778c3df
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2024-04-11 22:53:43 +0000

    CPU: Agrego logger

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <utils/hello.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     decir_hola("CPU");
+
+    char *modulo = "cpu";
+    t_log *logger = crear_logger(modulo);
+    log_info(logger, "Iniciando CPU ...");
+
     return 0;
 }
