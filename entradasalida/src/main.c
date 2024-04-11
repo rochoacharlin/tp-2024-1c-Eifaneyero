4424cf9709ab957837f38c7e8a0219a2530811ff
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2024-04-11 22:58:15 +0000

    E/S: Agrego logger

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <utils/hello.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     decir_hola("una Interfaz de Entrada/Salida");
+
+    char *modulo = "E/S";
+    t_log *logger = crear_logger(modulo);
+    log_info(logger, "Iniciando Interfaz de Entrada/Salida...");
+
     return 0;
 }
