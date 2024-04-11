8c87fca9819252bfb755a3fbbe4b84ccd2e5d31c
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2024-04-11 22:56:11 +0000

    Memoria: Agrego logger

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <utils/hello.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     decir_hola("Memoria");
+
+    char *modulo = "Memoria";
+    t_log *logger = crear_logger(modulo);
+    log_info(logger, "Iniciando Memoria ...");
+
     return 0;
 }
