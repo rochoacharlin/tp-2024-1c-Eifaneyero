84387cd847bded439163989fe458c946c249c8d7
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-04-12 00:31:16 +0000

    CPU: Inicio la config

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/funcionalidades_basicas.h>
 
 int main(int argc, char *argv[])
 {
-    decir_hola("CPU");
-
     char *modulo = "cpu";
     t_log *logger = crear_logger(modulo);
     log_info(logger, "Iniciando CPU ...");
+
+    t_config *config = iniciar_config(logger, "cpu.config");
 
     return 0;
 }
