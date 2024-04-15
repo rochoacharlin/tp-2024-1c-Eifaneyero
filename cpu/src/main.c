c26b0d172eee4a4386e52bea58b46b1fd5cbaf72
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-04-15 21:39:57 +0000

    CPU: Conecto con la Memoria

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/funcionalidades_basicas.h>
+#include <utils/comunicacion.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,11 @@ int main(int argc, char *argv[])
     log_info(logger, "Iniciando CPU ...");
 
     t_config *config = iniciar_config(logger, "cpu.config");
+
+    // conecta con la memoria
+    int conexion = conectar_a(config, logger, "IP_MEMORIA", "PUERTO_MEMORIA");
+
+    terminar_programa(conexion, logger, config);
 
     return 0;
 }
