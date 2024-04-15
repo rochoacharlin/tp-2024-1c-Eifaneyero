efb95ffc0f4d617028daa3b181f2e96df276cf1c
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-04-15 21:40:28 +0000

    Memoria: Espero la conexion de la CPU

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/funcionalidades_basicas.h>
+#include <utils/comunicacion.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,14 @@ int main(int argc, char *argv[])
     log_info(logger, "Iniciando Memoria ...");
 
     t_config *config = iniciar_config(logger, "memoria.config");
+
+    int server_fd = iniciar_servidor(config, logger, "PUERTO_ESCUCHA");
+    log_info(logger, "Memoria lista para recibir clientes");
+    int cliente_fd = esperar_cliente(logger, server_fd);
+    log_info(logger, "Se conectó un cliente!");
+
+    close(cliente_fd); // TODO es provisional
+    terminar_programa(server_fd, logger, config);
 
     return 0;
 }
