a5e2259be6bbf0114424aabd34f86734958a2d31
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2024-04-16 04:29:40 +0000

    Kernel:Intento conectar a Memoria
     Trato de usar a Kernel en modo cliente con la memoria , el problema es que no puedo probarlo ya que por parte la memoria me tira ' no se pudo crear memoria.config ',pero ambos funcionan en vs.code ( no se que pasa )

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -10,13 +10,38 @@ int main()
     log_info(logger, "Iniciando Kernel ...");
 
     t_config *config = iniciar_config(logger, "kernel.config");
+    int conexion;
+    // Provisorio
+    int z;
+    printf("Elija su modo de ejecución (numerico) \n - 1 (Servidor) \n - 2 (Cliente-Memoria)\n");
+    scanf("%d", &z);
+    switch (z)
+    {
+    case SERVIDOR:
 
-    int server_fd = iniciar_servidor(config, logger, "PUERTO_ESCUCHA");
-    log_info(logger, "Kernel listo para recibir clientes");
-    int cliente_fd = esperar_cliente(logger, server_fd);
-    log_info(logger, "Se conectó un cliente!");
+        int server_fd = iniciar_servidor(config, logger, "PUERTO_ESCUCHA");
+        log_info(logger, "Kernel listo para recibir clientes");
+        int cliente_fd = esperar_cliente(logger, server_fd);
+        log_info(logger, "Se conectó un cliente!");
 
-    close(cliente_fd); // TODO es provisional
-    terminar_programa(server_fd, logger, config);
+        close(cliente_fd); // TODO es provisional
+        terminar_programa(server_fd, logger, config);
+        break;
+
+    case C_MEMORIA:
+
+        conexion = conectar_a(config, logger, "IP_MEMORIA", "PUERTO_MEMORIA");
+        terminar_programa(conexion, logger, config);
+
+        break;
+
+    case C_CPU:
+        // TODO
+        break;
+
+    default:
+        break;
+    }
+
     return 0;
 }
--- utils/src/utils/funcionalidades_basicas.h
+++ utils/src/utils/funcionalidades_basicas.h
@@ -6,6 +6,9 @@
 #include <commons/log.h>
 #include <commons/string.h>
 #include <commons/config.h>
+#define SERVIDOR 0
+#define C_CPU 1
+#define C_MEMORIA 2
 
 t_log *crear_logger(char *modulo);
 t_config *iniciar_config(t_log *logger, char *ruta_archivo);
