81e2364b46ef4f03cd427156a170c82a164280cb
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-04-19 02:37:53 +0000

    CPU: Agrego conexión con memoria

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -11,10 +11,40 @@ int main(int argc, char *argv[])
 
     t_config *config = iniciar_config(logger, "cpu.config");
 
-    // conecta con la memoria
-    int conexion = conectar_a(config, logger, "IP_MEMORIA", "PUERTO_MEMORIA");
+    int modo_ejecucion;
 
-    terminar_programa(conexion, logger, config);
+    printf("Elija su modo de ejecución (numerico) \n - 0 (Servidor-Kernel) \n - 2 (Cliente-Memoria)\n");
+    scanf("%d", &modo_ejecucion);
+    switch (modo_ejecucion)
+    {
+    case SERVIDOR:
+        int puerto = -1;
+        int server_fd;
+        while (puerto != 0 && puerto != 1)
+        {
+            printf("Elija el puerto de escucha (numérico) \n - 0 Dispatch \n - 1 Interrupt \n ");
+            scanf("%d", &puerto);
+        }
+        if (puerto == 0)
+            server_fd = iniciar_servidor(config, logger, "PUERTO_ESCUCHA_DISPATCH");
+        else
+            server_fd = iniciar_servidor(config, logger, "PUERTO_ESCUCHA_INTERRUPT");
+
+        log_info(logger, "CPU lista para recibir clientes");
+        int cliente_fd = esperar_cliente(logger, server_fd);
+        log_info(logger, "Se conectó un cliente!");
+        close(cliente_fd); // TODO es provisional
+        terminar_programa(server_fd, logger, config);
+        break;
+
+    case C_MEMORIA:
+        int conexion = conectar_a(config, logger, "IP_MEMORIA", "PUERTO_MEMORIA");
+        terminar_programa(conexion, logger, config);
+        break;
+
+    default:
+        break;
+    }
 
     return 0;
 }
