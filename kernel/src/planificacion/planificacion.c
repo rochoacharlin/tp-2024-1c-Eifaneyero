213d26856a5a413d163fe5999cc6875d86b58f2f
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-02 19:57:49 +0000

    Kernel: Agrego planificacion a largo plazo

--- kernel/src/planificacion/planificacion.c
+++ kernel/src/planificacion/planificacion.c
@@ -1,10 +1,74 @@
 #include "planificacion.h"
 
+sem_t hay_pcbs_NEW;
+t_list *pcbs_en_NEW;
+sem_t sem_grado_multiprogramacion;
+t_list *pcbs_en_memoria;
+pthread_mutex_t mutex_lista_NEW;
+pthread_mutex_t mutex_lista_READY;
+
 t_list *pcbs_en_READY;
+sem_t hay_pcbs_READY;
 int32_t procesos_creados = 0;
-int grado_multiprogramacion;
 
 char *estados[5] = {"NEW", "READY", "EXEC", "BLOCKED", "EXIT"};
+
+void planificar_a_largo_plazo(void)
+{
+    printf("LLegue");
+    while (1)
+    {
+        printf("LLegue");
+        sem_wait(&hay_pcbs_NEW);
+        printf("Pase");
+        sem_wait(&sem_grado_multiprogramacion);
+        printf("Pase 2");
+
+        t_pcb *pcb = obtener_siguiente_pcb_READY();
+
+        // recibir_estructuras_iniciales_memoria(pcb);
+
+        estado anterior = pcb->estado;
+        pcb->estado = READY;
+        list_add(pcbs_en_memoria, pcb);
+        // log minimo y obligatorio
+        loggear_cambio_de_estado(pcb->PID, anterior, pcb->estado);
+        ingresar_pcb_a_READY(pcb);
+    }
+}
+
+t_pcb *obtener_siguiente_pcb_READY(void)
+{
+    pthread_mutex_lock(&mutex_lista_NEW);
+    t_pcb *pcb = desencolar_pcb(pcbs_en_NEW);
+    pthread_mutex_unlock(&mutex_lista_NEW);
+    return pcb;
+}
+
+void ingresar_pcb_a_READY(t_pcb *pcb)
+{
+    pthread_mutex_lock(&mutex_lista_READY);
+    encolar_pcb(pcbs_en_READY, pcb);
+
+    pthread_mutex_unlock(&mutex_lista_READY);
+
+    sem_post(&hay_pcbs_READY);
+
+    // log minimo y obligatorio
+    lista_PIDS = string_new();
+    mostrar_PIDS(pcbs_en_READY);
+    loggear_ingreso_a_READY(lista_PIDS);
+    free(lista_PIDS);
+}
+
+void ingresar_pcb_a_NEW(t_pcb *pcb)
+{
+    pthread_mutex_lock(&mutex_lista_NEW);
+    encolar_pcb(pcbs_en_NEW, pcb);
+    // log minimo y obligatorio
+    loggear_creacion_proceso(pcb);
+    pthread_mutex_unlock(&mutex_lista_NEW);
+}
 
 void planificar_a_corto_plazo_segun_algoritmo(void)
 {
@@ -16,11 +80,11 @@ void planificar_a_corto_plazo_segun_algoritmo(void)
     }
     else if (strcmp(algoritmo, "RR") == 0)
     {
-        // Solucion RR
+        // TODO: Solucion RR
     }
     else if (strcmp(algoritmo, "VRR") == 0)
     {
-        // Solucion VRR
+        // TODO: Solucion VRR
     }
     else
     {
@@ -31,19 +95,20 @@ void planificar_a_corto_plazo_segun_algoritmo(void)
 
 void planificar_a_corto_plazo(t_pcb *(*proximo_a_ejecutar)())
 {
-    // crear_colas_de_bloqueo();
+    crear_colas_de_bloqueo();
     while (1)
     {
-        // sem_wait(&hay_pcbs_READY);
+        sem_wait(&hay_pcbs_READY);
         t_pcb *pcb_proximo = proximo_a_ejecutar();
 
         estado anterior = pcb_proximo->estado;
         pcb_proximo->estado = EXEC;
 
+        // log minimo y obligatorio
         loggear_cambio_de_estado(pcb_proximo->PID, anterior, pcb_proximo->estado);
 
         // contexto_ejecucion = procesar_pcb(pcb_proximo);
-        // rafaga_CPU = contexto_ejecucion->rafaga_CPU_ejecutada;
+        // int rafaga_CPU = contexto_ejecucion->rafaga_CPU_ejecutada;
         // retorno_contexto(pcb_proximo, contexto_ejecucion);
     }
 }
@@ -53,7 +118,46 @@ t_pcb *proximo_a_ejecutar_segun_FIFO(void)
     return desencolar_pcb(pcbs_en_READY);
 }
 
+void inicializar_listas_planificacion(void)
+{
+    pcbs_en_NEW = list_create();
+    pcbs_en_READY = list_create();
+}
+
+void destruir_listas_planificacion(void)
+{
+    list_destroy_and_destroy_elements(pcbs_en_NEW, (void *)destruir_pcb);
+    list_destroy_and_destroy_elements(pcbs_en_READY, (void *)destruir_pcb);
+}
+
+void inicializar_semaforos_planificacion(void)
+{
+    pthread_mutex_init(&mutex_lista_NEW, NULL);
+    pthread_mutex_init(&mutex_lista_READY, NULL);
+    sem_init(&hay_pcbs_NEW, 0, 0);
+    sem_init(&hay_pcbs_READY, 0, 0);
+    sem_init(&sem_grado_multiprogramacion, 0, obtener_grado_multiprogramacion());
+}
+void destruir_semaforos_planificacion(void)
+{
+    pthread_mutex_destroy(&mutex_lista_NEW);
+    pthread_mutex_destroy(&mutex_lista_READY);
+    sem_close(&hay_pcbs_NEW);
+    sem_close(&hay_pcbs_READY);
+    sem_close(&sem_grado_multiprogramacion);
+}
+
 void loggear_cambio_de_estado(int PID, estado anterior, estado actual)
 {
     log_info(logger, "PID: <%d> - Estado Anterior: <%s> - Estado Actual: <%s>", PID, estados[anterior], estados[actual]);
+}
+
+void loggear_ingreso_a_READY(char *lista_PIDS)
+{
+    log_info(logger, "Cola Ready <%s>: [%s]", obtener_algoritmo_planificacion(), lista_PIDS);
+}
+
+void loggear_creacion_proceso(t_pcb *pcb)
+{
+    log_info(logger, "Se crea el proceso <%d> en NEW", pcb->PID);
 }
--- kernel/src/planificacion/planificacion.h
+++ kernel/src/planificacion/planificacion.h
@@ -4,25 +4,42 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include "pcb.h"
-#include "../main.h"
+#include "recursos.h"
+
+extern sem_t sem_grado_multiprogramacion;
+extern int grado_multiprogramacion;
+extern t_list *pcbs_en_NEW;
+extern sem_t hay_pcbs_NEW;
+extern pthread_mutex_t mutex_lista_NEW;
+extern pthread_mutex_t mutex_lista_READY;
+extern t_list *pcbs_en_memoria;
 
 extern sem_t hay_pcbs_READY;
-extern pthread_mutex_t mutex_lista_READY;
-extern sem_t sem_grado_multiprogramacion;
-
-extern int grado_multiprogramacion;
 extern int *instancias_recursos;
 
 extern t_list *pcbs_en_READY;
 extern char *estadosProcesos[5];
 
 // largo plazo
+void planificar_a_largo_plazo(void);
+void ingresar_pcb_a_NEW(t_pcb *pcb);
+t_pcb *obtener_siguiente_pcb_READY(void);
+void ingresar_pcb_a_READY(t_pcb *pcb);
+void inicializar_listas_planificacion(void);
+void destruir_listas_planificacion(void);
+
+// manejo de semaforos
+void inicializar_semaforos_planificacion(void);
+void destruir_semaforos_planificacion(void);
 
 // corto plazo
 void planificar_a_corto_plazo_segun_algoritmo(void);
 void planificar_a_corto_plazo(t_pcb *(*proximo_a_ejecutar)(void));
 t_pcb *proximo_a_ejecutar_segun_FIFO(void);
 
+// loggeos minimos y necesarios
 void loggear_cambio_de_estado(int PID, estado anterior, estado actual);
+void loggear_ingreso_a_READY(char *lista_PIDS);
+void loggear_creacion_proceso(t_pcb *pcb);
 
 #endif
