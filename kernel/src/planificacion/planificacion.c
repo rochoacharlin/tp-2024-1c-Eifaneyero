368c1ba8173c29cb0efdc60166a40b54e5456eae
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-02 02:42:01 +0000

    Kernel: Agrego funcionalidades de planificacion

--- kernel/src/planificacion/planificacion.c
+++ kernel/src/planificacion/planificacion.c
@@ -1,12 +1,18 @@
 #include "planificacion.h"
 
-void planificar_a_corto_plazo_segun_algoritmo()
+t_list *pcbs_en_READY;
+int32_t procesos_creados = 0;
+int grado_multiprogramacion;
+
+char *estados[5] = {"NEW", "READY", "EXEC", "BLOCKED", "EXIT"};
+
+void planificar_a_corto_plazo_segun_algoritmo(void)
 {
     char *algoritmo = obtener_algoritmo_planificacion();
 
     if (strcmp(algoritmo, "FIFO") == 0)
     {
-        // Solucion FIFO
+        planificar_a_corto_plazo(proximo_a_ejecutar_segun_FIFO);
     }
     else if (strcmp(algoritmo, "RR") == 0)
     {
@@ -21,4 +27,33 @@ void planificar_a_corto_plazo_segun_algoritmo()
         log_error(logger, "Algoritmo invalido. Debe ingresar FIFO, RR o VRR");
         abort();
     }
+}
+
+void planificar_a_corto_plazo(t_pcb *(*proximo_a_ejecutar)())
+{
+    // crear_colas_de_bloqueo();
+    while (1)
+    {
+        // sem_wait(&hay_pcbs_READY);
+        t_pcb *pcb_proximo = proximo_a_ejecutar();
+
+        estado anterior = pcb_proximo->estado;
+        pcb_proximo->estado = EXEC;
+
+        loggear_cambio_de_estado(pcb_proximo->PID, anterior, pcb_proximo->estado);
+
+        // contexto_ejecucion = procesar_pcb(pcb_proximo);
+        // rafaga_CPU = contexto_ejecucion->rafaga_CPU_ejecutada;
+        // retorno_contexto(pcb_proximo, contexto_ejecucion);
+    }
+}
+
+t_pcb *proximo_a_ejecutar_segun_FIFO(void)
+{
+    return desencolar_pcb(pcbs_en_READY);
+}
+
+void loggear_cambio_de_estado(int PID, estado anterior, estado actual)
+{
+    log_info(logger, "PID: <%d> - Estado Anterior: <%s> - Estado Actual: <%s>", PID, estados[anterior], estados[actual]);
 }
--- kernel/src/planificacion/planificacion.h
+++ kernel/src/planificacion/planificacion.h
@@ -1,9 +1,28 @@
 #ifndef KERNEL_PLANIFICACION_H
 #define KERNEL_PLANIFICACION_H
 
+#include <pthread.h>
+#include <semaphore.h>
 #include "pcb.h"
 #include "../main.h"
 
-void planificar_a_corto_plazo_segun_algoritmo();
+extern sem_t hay_pcbs_READY;
+extern pthread_mutex_t mutex_lista_READY;
+extern sem_t sem_grado_multiprogramacion;
+
+extern int grado_multiprogramacion;
+extern int *instancias_recursos;
+
+extern t_list *pcbs_en_READY;
+extern char *estadosProcesos[5];
+
+// largo plazo
+
+// corto plazo
+void planificar_a_corto_plazo_segun_algoritmo(void);
+void planificar_a_corto_plazo(t_pcb *(*proximo_a_ejecutar)(void));
+t_pcb *proximo_a_ejecutar_segun_FIFO(void);
+
+void loggear_cambio_de_estado(int PID, estado anterior, estado actual);
 
 #endif
