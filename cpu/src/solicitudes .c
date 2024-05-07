5ef8830827f49f492b6656ee56568bb904547b0a
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-05-07 07:04:20 +0000

    CPU: Agrego solicitudes.c

---
+++ cpu/src/solicitudes .c
@@ -0,0 +1,17 @@
+#include "interface_cpu.h"
+
+void solicitar_lectura_de_instruccion(int conexion_memoria, char *path_archivo_instruccion, uint32_t desplazamiento) // faltan funciones de serializacion y envio de mensajes
+{
+    /*
+    t_solicitud_de_instruccion solicitud = crear_solicitud_de_instruccion();
+
+    solicitud->path = path_archivo_instruccion;
+    solicitud->desplazamiento;
+
+    t_paquete paquete = serializar(solicitud, &procesar_solicitud);
+
+    enviar_mensaje(conexion_memoria, paquete);
+
+    destruir_solicitud_de_instruccion(solicitud);
+    */
+}
