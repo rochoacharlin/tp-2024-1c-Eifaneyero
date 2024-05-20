8926e597b2244f8917be2f6060bc2b2975240128
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-05-20 22:13:31 +0000

    CPU:  Agrego mejora en solicitud de lectura

--- cpu/src/solicitudes.c
+++ cpu/src/solicitudes.c
@@ -1,17 +1,19 @@
 #include "interface_cpu.h"
 
-void solicitar_lectura_de_instruccion(int conexion_memoria, char *path_archivo_instruccion, uint32_t desplazamiento) // faltan funciones de serializacion y envio de mensajes
+void solicitar_lectura_de_instruccion(int conexion_memoria, uint32_t desplazamiento) // TODO: faltan implementar bien las fuciones
 {
-    /*
     t_solicitud_de_instruccion solicitud = crear_solicitud_de_instruccion();
-
-    solicitud->path = path_archivo_instruccion;
     solicitud->desplazamiento;
 
-    t_paquete paquete = serializar(solicitud, &procesar_solicitud);
+    t_paquete paquete = crear_paquete();
+    paquete->codigo_operacion = SOLICITUD_INSTRUCCION;
+    // TODO: falta hacer tamanio_solicitud_de_instruccion, procesar_solicitud_instruccion
+    paquete->buffer = serializar_estructura(solicitud, tamanio_solicitud_instruccion, serealizar_solicitud_instruccion);
+
+    t_buffer heap = serializar_paquete(paquete);
 
     enviar_mensaje(conexion_memoria, paquete);
 
     destruir_solicitud_de_instruccion(solicitud);
-    */
+    destruir_paquete(paquete);
 }
