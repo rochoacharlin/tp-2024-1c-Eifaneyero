106f692d117b0a9db99b321f854e24fbe1907764
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-05-23 06:33:33 +0000

    CPU: Agrego codigo de operacion como argumento de crear paquete

--- cpu/src/solicitudes.c
+++ cpu/src/solicitudes.c
@@ -3,9 +3,7 @@
 void solicitar_lectura_de_instruccion(int conexion_memoria, uint32_t desplazamiento) // TODO: faltan implementar bien las fuciones
 {
     t_solicitud_de_instruccion *solicitud = crear_solicitud_de_instruccion();
-    solicitud->desplazamiento;
-    t_paquete *paquete = crear_paquete();
-    paquete->codigo_operacion = 0; // TODO: ENUM OPERACIONES "SOLICITUD_INSTRUCCION"
+    t_paquete *paquete = crear_paquete(SOLICITUD_INSTRUCCION);
     // TODO: falta hacer tamanio_solicitud_de_instruccion, procesar_solicitud_instruccion
     // paquete->buffer = serializar_estructura(solicitud, tamanio_solicitud_instruccion, serealizar_solicitud_instruccion)
 
