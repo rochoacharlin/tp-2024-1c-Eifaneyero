408f5e97afa680c84f2e24aca20da8b814761b4d
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-05-23 09:45:19 +0000

    Memoria : Creo carpera de conexiones con su .h

---
+++ memoria/conexiones/conexiones.h
@@ -0,0 +1,9 @@
+#ifndef MEMORIA_CONEXIONES_H
+#define MEMORIA_CONEXIONES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <utils/funcionalidades_basicas.h>
+#include <utils/comunicacion.h>
+
+#endif
