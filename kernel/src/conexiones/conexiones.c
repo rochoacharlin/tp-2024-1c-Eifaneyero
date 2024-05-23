1235874b4c4bc6504f876a9442bb87abc76a36b1
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-05-23 08:40:48 +0000

    Kernel: Agrego funcion gestionar_ejecucion_proceso

--- kernel/src/conexiones/conexiones.c
+++ kernel/src/conexiones/conexiones.c
@@ -1,5 +1,8 @@
 #include "conexiones.h"
-// #include "./contexto_ejecucion.h"
+
+t_buffer *buffer_contexto;
+int conexion_a_cpu_dispatch;  // socket
+int conexion_a_cpu_interrupt; // socket
 
 void servidor(void)
 {
@@ -14,16 +17,16 @@ void servidor(void)
 
 void conexion_interrupt_con_CPU(void)
 {
-    int conexion = crear_conexion(logger_propio, obtener_ip_cpu(), obtener_puerto_cpu_interrupt());
+    int conexion_a_cpu_interrupt = crear_conexion(logger_propio, obtener_ip_cpu(), obtener_puerto_cpu_interrupt());
     int32_t handshake = 5;
-    int handshake_respuesta = handshake_cliente(logger_propio, conexion, handshake);
+    int handshake_respuesta = handshake_cliente(logger_propio, conexion_a_cpu_interrupt, handshake);
 }
 
 void conexion_dispatch_con_CPU(void)
 {
-    int conexion = crear_conexion(logger_propio, obtener_ip_cpu(), obtener_puerto_cpu_dispatch());
+    int conexion_a_cpu_dispatch = crear_conexion(logger_propio, obtener_ip_cpu(), obtener_puerto_cpu_dispatch());
     int32_t handshake = 5;
-    int handshake_respuesta = handshake_cliente(logger_propio, conexion, handshake);
+    int handshake_respuesta = handshake_cliente(logger_propio, conexion_a_cpu_dispatch, handshake);
 }
 
 void conexion_con_memoria(void)
@@ -33,96 +36,16 @@ void conexion_con_memoria(void)
     int handshake_respuesta = handshake_cliente(logger_propio, conexion, handshake);
 }
 
-/*
-t_contexto *gestionar_ejecucion_proceso(t_pcb *proceso_en_ejecucion)
+t_contexto_ejecucion *gestionar_ejecucion_proceso(t_pcb *proceso_en_ejecucion) // CPU ejecuta luego de enviar contexto actualizado
 {
     if (contexto_ejecucion != NULL)
-        destruir_contexto(); // Verifica y destruye contexto previo
+        destruir_contexto();
     iniciar_contexto();
-
-    buffer_contexto = malloc(sizeof(t_buffer));
-
-    asignar_pcb_a_contexto(proceso_en_ejecucion);
-
-    enviar_contexto_actualizado(conexion_a_cpu); //Variable que contiene socket de conexion con CPU
-
-    if (recibir_operacion_de_cpu() < 0) // TODO: recibir_operacion(conexion_a_cpu);
-    {
+    asignar_valores_pcb_a_contexto(proceso_en_ejecucion);
+    enviar_contexto_actualizado(conexion_a_cpu_dispatch); // Envió para realizar ciclo de instrucción
+    if (recibir_operacion(conexion_a_cpu_dispatch) < 0)   // Solo verifico recepción de respuesta, indiferente al opCode
         error("Se desconectó el CPU.");
-    }
-
-    recibir_contexto_actualizado(conexion_a_cpu);
-
+    recibir_contexto_y_actualizar_global(conexion_a_cpu_dispatch); // Da por sentado que opCode recibido es CONTEXTO_EJECUCION
     actualizar_pcb(proceso_en_ejecucion);
-    free(buffer_contexto);
     return contexto_ejecucion;
 }
-*/
-
-/*
-void asignar_pcb_a_contexto(t_pcb* proceso) {
-    list_destroy_and_destroy_elements(contexto_ejecucion->instrucciones, free);
-    contexto_ejecucion->pid = proceso->pid;
-    dictionary_destroy_and_destroy_elements(contexto_ejecucion->registros_cpu, free);
-    contexto_ejecucion->registros_cpu = registros_de_la_cpu(proceso->registros_cpu); //TODO: registros_del_cpu
-}
-*/
-
-/*
-
-void actualizarPCB(t_pcb* proceso){
-    proceso->pid = contexto_ejecucion->pid;
-    // proceso->program_counter = contexto_ejecucion->program_counter;
-    dictionary_destroy_and_destroy_elements(proceso->registros_cpu, free);
-    proceso->registros_cpu = registros_de_la_cpu(contexto_ejecucion->registros_cpu);
-
-}
-
-*/
-
-/*
-
-//Copia diccionario de registros de CPU
-t_dictionary *registros_de_la_cpu(t_dictionary *a_copiar) {
-    t_dictionary *copia = dictionary_create();
-
-    char* AX = malloc(sizeof(char) * (8 + 1));
-    char* BX = malloc(sizeof(char) * (8 + 1));
-    char* CX = malloc(sizeof(char) * (8 + 1));
-    char* DX = malloc(sizeof(char) * (8 + 1));
-    char* EAX = malloc(sizeof(char) * (32 + 1));
-    char* EBX = malloc(sizeof(char) * (32 + 1));
-    char* ECX = malloc(sizeof(char) * (32 + 1));
-    char* EDX = malloc(sizeof(char) * (32 + 1));
-    char* PC = malloc(sizeof(char) * (32 + 1));
-    char* SI = malloc(sizeof(char) * (32 + 1));
-    char* DI = malloc(sizeof(char) * (32 + 1));
-
-    strncpy(AX, (char *)dictionary_get(aCopiar, "AX"), 8 + 1);
-    strncpy(BX, (char *)dictionary_get(aCopiar, "BX"), 8 + 1);
-    strncpy(CX, (char *)dictionary_get(aCopiar, "CX"), 8 + 1);
-    strncpy(DX, (char *)dictionary_get(aCopiar, "DX"), 8 + 1);
-    strncpy(EAX, (char *)dictionary_get(aCopiar, "EAX"), 32 + 1);
-    strncpy(EBX, (char *)dictionary_get(aCopiar, "EBX"), 32 + 1);
-    strncpy(ECX, (char *)dictionary_get(aCopiar, "ECX"), 32 + 1);
-    strncpy(EDX, (char *)dictionary_get(aCopiar, "EDX"), 32 + 1);
-    strncpy(PC, (char *)dictionary_get(aCopiar, "PC"), 32 + 1);
-    strncpy(SI, (char *)dictionary_get(aCopiar, "SI"), 32 + 1);
-    strncpy(DI, (char *)dictionary_get(aCopiar, "DI"), 32 + 1);
-
-    dictionary_put(copia, "AX", AX);
-    dictionary_put(copia, "BX", BX);
-    dictionary_put(copia, "CX", CX);
-    dictionary_put(copia, "DX", DX);
-    dictionary_put(copia, "EAX", EAX);
-    dictionary_put(copia, "EBX", EBX);
-    dictionary_put(copia, "ECX", ECX);
-    dictionary_put(copia, "EDX", EDX);
-    dictionary_put(copia, "PC", PC);
-    dictionary_put(copia, "SI", SI);
-    dictionary_put(copia, "DI", DI);
-
-    return copia;
-}
-
-*/
--- kernel/src/conexiones/conexiones.h
+++ kernel/src/conexiones/conexiones.h
@@ -1,12 +1,24 @@
 #ifndef KERNEL_CONEXIONES_H
 #define KERNEL_CONEXIONES_H
 
-#include "configuraciones.h"
-#include <utils/comunicacion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../configuraciones.h"
+#include "../planificacion/pcb.h"
+#include "utils/comunicacion.h"
+#include "utils/serializacion.h"
+#include "utils/contexto_ejecucion.h"
+#include <commons/collections/list.h>
+#include <commons/collections/dictionary.h>
 
 void servidor(void);
 void conexion_dispatch_con_CPU(void);
 void conexion_interrupt_con_CPU(void);
 void conexion_con_memoria(void);
+
+// Envío contexto a CPU a partir de PCB y quedo a espera de OpCode
+// Luego recibo contexto actualizado y actualizo PCB del proceso
+t_contexto_ejecucion *gestionar_ejecucion_proceso(t_pcb *proceso_en_ejecucion);
 
 #endif
