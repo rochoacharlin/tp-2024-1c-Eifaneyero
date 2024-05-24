4f8f134161f2719030a5b76439a08d21e897dfe6
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-05-24 08:48:36 +0000

    Memoria: Agrego logica de Solicitud de Istruccion y algunas alternativas provisorias a la Deserializacion

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,9 +1,16 @@
 
 #include "configuraciones.h"
+#include <utils/serializacion.h>
+#include <utils/estructuras_compartidas.h>
+#include "conexiones/conexiones.h"
 
 t_log *logger_obligatorio;
 t_log *logger_propio;
 t_config *config;
+// t_list *valores_paquete;
+
+t_paquete *paquete_recibir(int socket);
+t_paquete *paquete_deserializar(void *buffer, int size);
 
 int main(int argc, char *argv[])
 {
@@ -22,22 +29,70 @@ int main(int argc, char *argv[])
     int handshake_respuesta = handshake_servidor(logger_propio, cliente_fd, handshake_esperado);
 
     // Por cada Hilo
-    /*recibir_paquete(cliente_fd);
+
+    t_paquete *paquete = paquete_recibir(cliente_fd);
+
+    op_code codigo_operacion = paquete->codigo_operacion;
+
+    log_info(logger_propio, "se recibio un paquete");
+
+    printf("el codigo operacion es: %d \n", codigo_operacion);
 
     switch (codigo_operacion)
     {
     case (SOLICITUD_INSTRUCCION):
-        // TODO: proceso de busqueda y envio de instrucciones
+
+        t_solicitud_de_instruccion *solicitud = crear_solicitud_de_instruccion();
+        deserializar_solicitud_de_instruccion(solicitud, paquete);
+
+        log_info(logger_propio, "se recibio una solicitud desde cpu \n el valor del PC es:", solicitud->desplazamiento);
+
+        /*t_instruccion instruccion = leerInstruccion(solicitud->desplazamiento);
+         enviar a cpu la instruccion
+        */
+
         break;
 
     default:
         break;
     }
-    */
+
+    eliminar_paquete(paquete);
 
     close(cliente_fd); // TODO es provisional
     terminar_programa(server_fd, logger_propio, config);
     log_destroy(logger_obligatorio);
 
     return 0;
+}
+
+// alternativa a serializar
+
+t_paquete *paquete_recibir(int socket)
+{
+    int size;
+    void *buffer;
+    t_list *valores = list_create();
+    int tamanio;
+
+    buffer = recibir_buffer(socket, &size);
+    return paquete_deserializar(buffer, size);
+}
+
+t_paquete *paquete_deserializar(void *buffer, int size)
+{
+    t_paquete *paquete = malloc(sizeof(t_paquete));
+    int desplazamiento = 0;
+
+    memcpy(&(paquete->codigo_operacion), buffer + desplazamiento, sizeof(int));
+    desplazamiento += sizeof(int);
+
+    paquete->buffer = malloc(sizeof(t_buffer));
+
+    memcpy(&(paquete->buffer->size), buffer + desplazamiento, sizeof(int));
+    desplazamiento += sizeof(int);
+    memcpy(paquete->buffer->stream, buffer + desplazamiento, size);
+    desplazamiento += sizeof(int);
+
+    return paquete;
 }
