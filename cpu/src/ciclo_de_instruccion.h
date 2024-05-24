6d32caf24ae746c2e10558fabfe42ab1ae5340b2
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-05-24 18:12:06 +0000

    CPU: agrego listado de comandos para conectar id de instruccion con su string

--- cpu/src/ciclo_de_instruccion.h
+++ cpu/src/ciclo_de_instruccion.h
@@ -37,6 +37,27 @@ typedef enum
     EXIT
 } t_id;
 
+char *comandos[] = {
+    [SET] = "SET",
+    [MOV_IN] = "MOV_IN",
+    [MOV_OUT] = "MOV_OUT",
+    [SUM] = "SUM",
+    [SUB] = "SUB",
+    [JNZ] = "JNZ",
+    [RESIZE] = "RESIZE",
+    [COPY_STRING] = "COPY_STRING",
+    [WAIT] = "WAIT",
+    [SIGNAL] = "SIGNAL",
+    [IO_GEN_SLEEP] = "IO_GEN_SLEEP",
+    [IO_STDIN_READ] = "IO_STDIN_READ",
+    [IO_STDOUT_WRITE] = "IO_STDOUT_WRITE",
+    [IO_FS_CREATE] = "IO_FS_CREATE",
+    [IO_FS_DELETE] = "IO_FS_DELETE",
+    [IO_FS_TRUNCATE] = "IO_FS_TRUNCATE",
+    [IO_FS_WRITE] = "IO_FS_WRITE",
+    [IO_FS_READ] = "IO_FS_READ",
+    [EXIT] = "EXIT"};
+
 typedef struct
 {
     t_id id;
