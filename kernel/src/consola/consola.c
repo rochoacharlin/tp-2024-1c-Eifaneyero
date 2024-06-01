0e6d42f4b125a5a389263ab98c659bcc9a90fbbb
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-06-01 23:01:29 +0000

    Kernel: Cambios de organizacion en la consola

--- kernel/src/consola/consola.c
+++ kernel/src/consola/consola.c
@@ -20,7 +20,6 @@ void consola_interactiva(void)
         buscar_y_ejecutar_comando(token);
         free(leido);
 
-        // sem_wait(&termina_comando);
         sleep(1);
         leido = readline("> ");
         token = strtok(leido, " ");
@@ -29,112 +28,6 @@ void consola_interactiva(void)
     free(leido);
     destruir_semaforos_planificacion();
     destruir_listas_planificacion();
-}
-
-void ejecutar_script(char *path)
-{
-    FILE *archivo;
-
-    if (path == NULL)
-        archivo = fopen(path, "r");
-
-    if (archivo == NULL)
-    {
-        log_error(logger_propio, "Error al abrir el archivo.");
-        return;
-    }
-
-    char comando[256];
-
-    while (!feof(archivo))
-    {
-        fgets(comando, sizeof(comando), archivo);
-        char *token = strtok(comando, " ");
-
-        // Eliminar el carácter de nueva línea si está presente
-        size_t longitud = strlen(comando);
-        if (longitud > 0 && comando[longitud - 1] == '\n')
-            comando[longitud - 1] = '\0';
-
-        buscar_y_ejecutar_comando(token);
-    }
-
-    fclose(archivo);
-}
-
-void iniciar_proceso(char *path)
-{
-    // COMPLETAR: Falta toda la parte de la memoria y filesystem.
-    t_pcb *pcb = crear_pcb();
-    ingresar_pcb_a_NEW(pcb);
-}
-
-void finalizar_proceso(char *PID)
-{
-    // TODO: Liberar recursos, archivos y memoria
-    t_pcb *pcb = NULL;
-
-    if (PID != NULL)
-        pcb = buscar_pcb_por_PID(pcbs_en_memoria, (uint32_t)atoi(PID));
-
-    if (pcb == NULL)
-        log_error(logger_propio, "No existe un PCB con ese PID.");
-    else
-    {
-        remover_pcb_de_listas_globales(pcb);
-        pcb->estado = EXIT;
-
-        pthread_mutex_lock(&mutex_lista_EXIT);
-        list_add(pcbs_en_EXIT, pcb);
-        pthread_mutex_unlock(&mutex_lista_EXIT);
-
-        sem_post(&sem_grado_multiprogramacion);
-    }
-}
-
-void detener_planificacion(void)
-{
-    // COMPLETAR: El proceso que se encuentra en ejecución NO es desalojado,
-    //            pero una vez que salga de EXEC se va a pausar el manejo de su motivo de desalojo.
-    sem_wait(&planificacion_liberada);
-    sem_post(&planificacion_pausada);
-}
-
-void iniciar_planificacion(void)
-{
-    sem_wait(&planificacion_pausada); // para asegurar que libere la planificacion cuando este pausada.
-    sem_post(&planificacion_liberada);
-}
-
-void cambiar_grado_multiprogramacion(char *valor_deseado)
-{
-    if (valor_deseado == NULL)
-        log_error(logger_propio, "No se indico el numero de grado de multiprogramacion");
-    else
-    {
-        int valor_resultante, valor_actual, valor_inicial;
-        valor_inicial = obtener_grado_multiprogramacion();
-        sem_getvalue(&sem_grado_multiprogramacion, &valor_actual);
-
-        valor_resultante = atoi(valor_deseado) - valor_inicial + valor_actual;
-        cambiar_valor_de_semaforo(&sem_grado_multiprogramacion, valor_resultante);
-        config_set_value(config, "GRADO_MULTIPROGRAMACION", valor_deseado);
-
-        sem_getvalue(&sem_grado_multiprogramacion, &valor_actual);
-        log_info(logger_propio, "Grado multiprogramacion actual: %d", valor_actual);
-
-        // TODO: En caso de que se tengan más procesos ejecutando que lo que permite el grado de
-        //       multiprogramación, no se tomarán acciones sobre los mismos y se esperará su finalización normal.
-    }
-}
-
-void listar_procesos_por_cada_estado(void)
-{
-    listar_procesos_por_estado("NEW", pcbs_en_NEW);
-    listar_procesos_por_estado("READY", pcbs_en_READY);
-    printf("EXEC: %s\n", pcb_en_EXEC != NULL ? string_itoa(pcb_en_EXEC->PID) : " ");
-    listar_procesos_por_estado("BLOCKED", pcbs_en_BLOCKED);
-    listar_procesos_por_estado("EXIT", pcbs_en_EXIT);
 }
 
 void listar_procesos_por_estado(char *estado, t_list *lista)
--- kernel/src/consola/consola.h
+++ kernel/src/consola/consola.h
@@ -1,7 +1,6 @@
 #ifndef KERNEL_CONSOLA_H_
 #define KERNEL_CONSOLA_H_
 
-// #include <string.h>
 #include <readline/readline.h>
 #include <stdlib.h>
 #include "../planificacion/pcb.h"
@@ -11,7 +10,6 @@
 extern sem_t planificacion_liberada;
 extern sem_t planificacion_pausada;
 extern sem_t sem_grado_multiprogramacion;
-extern sem_t termina_comando;
 
 typedef struct t_comando
 {
@@ -20,6 +18,8 @@ typedef struct t_comando
 } t_comando;
 
 void consola_interactiva(void);
+
+// comandos
 void ejecutar_script(char *path);
 void iniciar_proceso(char *path);
 void finalizar_proceso(char *PID);
@@ -27,8 +27,9 @@ void detener_planificacion(void);
 void iniciar_planificacion(void);
 void cambiar_grado_multiprogramacion(char *valor);
 void listar_procesos_por_cada_estado(void);
-void listar_procesos_por_estado(char *estado, t_list *lista);
 
+// funciones utiles
+void listar_procesos_por_estado(char *estado, t_list *lista);
 void buscar_y_ejecutar_comando(char *token);
 void cambiar_valor_de_semaforo(sem_t *sem, int valor_resultante);
 void remover_pcb_de_listas_globales(t_pcb *pcb);
