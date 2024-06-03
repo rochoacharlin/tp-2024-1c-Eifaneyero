0fe5dca63dd3dbd518c4d18d1c2bbea79bf577a1
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-06-03 07:16:59 +0000

    CPU: agrego validación a recibo de contexto por parte de CPU

--- cpu/src/conexiones/conexiones.c
+++ cpu/src/conexiones/conexiones.c
@@ -4,7 +4,7 @@ int conexion_cpu_memoria;
 int conexion_cpu_kernel_dispatch;
 int conexion_cpu_kernel_interrupt;
 
-pthread_mutex_t *mutex_interrupt;
+// pthread_mutex_t *mutex_interrupt;
 
 pthread_t th_dispatch;
 pthread_t th_interrupt;
@@ -16,8 +16,8 @@ void iniciar_servidor_dispatch(void)
     conexion_cpu_kernel_dispatch = esperar_cliente(logger_propio, server_fd);
     log_info(logger_propio, "Se conectó un cliente!");
 
-    int32_t handshake_esperado = 4;
-    int handshake_respuesta = handshake_servidor(logger_propio, conexion_cpu_kernel_dispatch, handshake_esperado);
+    // int32_t handshake_esperado = 4;
+    // int handshake_respuesta = handshake_servidor(logger_propio, conexion_cpu_kernel_dispatch, handshake_esperado);
 }
 
 void iniciar_servidor_interrupt(void)
@@ -27,15 +27,15 @@ void iniciar_servidor_interrupt(void)
     conexion_cpu_kernel_interrupt = esperar_cliente(logger_propio, server_fd);
     log_info(logger_propio, "Se conectó un cliente!");
 
-    int32_t handshake_esperado = 4;
-    int handshake_respuesta = handshake_servidor(logger_propio, conexion_cpu_kernel_interrupt, handshake_esperado);
+    // int32_t handshake_esperado = 4;
+    // int handshake_respuesta = handshake_servidor(logger_propio, conexion_cpu_kernel_interrupt, handshake_esperado);
 }
 
 void iniciar_conexion_memoria(void)
 {
     conexion_cpu_memoria = crear_conexion(logger_propio, obtener_ip_memoria(), obtener_puerto_memoria());
-    int32_t handshake = 1;
-    int handshake_respuesta = handshake_cliente(logger_propio, conexion_cpu_memoria, handshake);
+    // int32_t handshake = 1;
+    // int handshake_respuesta = handshake_cliente(logger_propio, conexion_cpu_memoria, handshake);
 }
 
 void iniciar_conexiones()
@@ -57,9 +57,20 @@ void atender_dispatch()
 
     while (1)
     {
-        t_contexto *contexto = recibir_contexto(conexion_cpu_kernel_dispatch);
-        ciclo_de_instruccion(contexto);
-        destruir_contexto(contexto);
+        int opCode = recibir_operacion(conexion_cpu_kernel_dispatch);
+        log_info(logger_propio, "opCode: %d", opCode);
+        if (opCode == CONTEXTO_EJECUCION)
+        {
+            log_info(logger_propio, "Contexto recibido por CPU");
+            t_contexto *contexto = recibir_contexto(conexion_cpu_kernel_dispatch);
+            ciclo_de_instruccion(contexto);
+            destruir_contexto(contexto);
+        }
+        else
+        {
+            log_info(logger_propio, "Enviaste a CPU algo que no es un contexto");
+            break;
+        }
     }
 }
 
--- cpu/src/conexiones/conexiones.h
+++ cpu/src/conexiones/conexiones.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
 #include "../configuraciones.h"
 #include <utils/comunicacion/comunicacion.h>
 #include "../../cpu/src/ciclo_de_instruccion.h" //Compartir hay_interrupcion y motivo de instrucccion
