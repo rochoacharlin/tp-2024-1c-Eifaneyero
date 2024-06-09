4a7402c21701bc4d92a01eb8e38ea1311ce24bb3
Gauna Somá, Facundo <fgaunasoma@frba.utn.edu.ar>
2024-06-09 04:27:20 -0300

    CPU: Cambio parametros de solicitar_lectura_de_instruccion()

--- cpu/src/interface_cpu.h
+++ cpu/src/interface_cpu.h
@@ -8,7 +8,8 @@
 #include <utils/estructuras_compartidas/contexto_ejecucion.h>
 #include "conexiones/conexiones.h"
 
-void solicitar_lectura_de_instruccion(uint32_t desplazamiento);
+// OpCode | Size PID | PID | Size PC | PC
+void solicitar_lectura_de_instruccion(uint32_t PID, uint32_t PC);
 void solicitar_marco_memoria(uint32_t PID, int pagina);
 
 int recibir_marco_memoria();
