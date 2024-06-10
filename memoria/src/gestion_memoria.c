9fb6aa5e70b14149f466a8dfc7cad391d5400edb
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-06-10 11:04:45 +0000

    Memoria: gestion. Agrego leer_espacio_usuario()  y escribir_espacio_usuario()

--- memoria/src/gestion_memoria.c
+++ memoria/src/gestion_memoria.c
@@ -5,6 +5,8 @@ t_dictionary *indice_tablas;
 t_dictionary *indice_instrucciones;
 t_bitarray *marcos_libres;
 
+pthread_mutex_t mutex_memoria;
+
 void inicializar_memoria(void)
 {
     espacio_usuario = malloc(obtener_tam_memoria());
@@ -15,4 +17,27 @@ void inicializar_memoria(void)
     marcos_libres = crear_marcos_libres();
 
     indice_instrucciones = crear_indice_de_instrucciones();
+
+    pthread_mutex_init(&mutex_memoria, NULL);
+}
+
+// No sé donde ponerles:
+// Interpreto que los registros de memoria son de 32 bits
+uint32_t leer_espacio_usuario(uint32_t direccion)
+{
+    uint32_t valor;
+
+    pthread_mutex_lock(&mutex_memoria);
+    memcpy(&valor, espacio_usuario + direccion, sizeof(uint32_t));
+    pthread_mutex_unlock(&mutex_memoria);
+
+    return valor;
+}
+
+// Escribo sin importar si me voy de la página.
+void escribir_espacio_usuario(uint32_t direccion, uint32_t valor)
+{
+    pthread_mutex_lock(&mutex_memoria);
+    memcpy(espacio_usuario + direccion, &valor, sizeof(int));
+    pthread_mutex_unlock(&mutex_memoria);
 }
