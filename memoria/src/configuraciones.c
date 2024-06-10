ac299a0a02dc625cf66297a272636ccfe3707555
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-06-10 11:06:36 +0000

    Memoria: Agrego logs faltantes

--- memoria/src/configuraciones.c
+++ memoria/src/configuraciones.c
@@ -2,7 +2,7 @@
 
 void loggear_creacion_destruccion_tabla_de_paginas(uint32_t PID, int cantidad_paginas)
 {
-    // TODO
+    log_info(logger_obligatorio, "PID: <%d> - Tamaño: <%d>", PID, cantidad_paginas);
 }
 
 void loggear_acceso_tablas_de_paginas(uint32_t PID, int pagina, int marco)
@@ -20,7 +20,12 @@ void loggear_reduccion_de_proceso(uint32_t PID, int tamanio_actual, int tamanio_
     log_info(logger_obligatorio, "PID: <%d> - Tamaño Actual: <%d> - Tamaño a Reducir: <%d>", PID, tamanio_actual, tamanio_a_reducir);
 }
 
-void loggear_acceso_a_espacio_de_usuario(uint32_t PID, int accion, int direccion_fisica, int tamanio)
+void loggear_lectura_espacio_de_usuario(uint32_t PID, int direccion_fisica, int tamanio)
 {
-    // TODO
+    log_info(logger_obligatorio, "PID: <%d> - Accion: <LEER> - Direccion fisica: <%d> - Tamaño <%d>", PID, direccion_fisica, tamanio);
+}
+
+void loggear_escritura_espacio_de_usuario(uint32_t PID, int direccion_fisica, int tamanio)
+{
+    log_info(logger_obligatorio, "PID: <%d> - Accion: <ESCRIBIR> - Direccion fisica: <%d> - Tamaño <%d>", PID, direccion_fisica, tamanio);
 }
