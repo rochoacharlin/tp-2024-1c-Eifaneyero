c3d0911f165c469efcb5b31dd25948a2fc6352bb
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-06-18 20:25:43 +0000

    Memoria: se agregó un mensaje para cuando se conecta algún cliente

--- memoria/src/conexiones/conexiones.c
+++ memoria/src/conexiones/conexiones.c
@@ -20,18 +20,21 @@ void iniciar_conexiones()
         switch (codigo_de_operacion)
         {
         case CONEXION_IO:
+            log_info(logger_propio, "Se conectó una IO");
             pthread_t hilo_io;
             pthread_create(&hilo_io, NULL, (void *)atender_io, &conexion_entrante);
             pthread_join(hilo_io, NULL);
             break;
 
         case CONEXION_KERNEL:
+            log_info(logger_propio, "Se conectó el kernel");
             pthread_t hilo_kernel;
             pthread_create(&hilo_kernel, NULL, (void *)atender_kernel, &conexion_entrante);
             pthread_detach(hilo_kernel);
             break;
 
         case CONEXION_CPU:
+            log_info(logger_propio, "Se conectó la CPU");
             pthread_t hilo_cpu;
             pthread_create(&hilo_cpu, NULL, (void *)atender_cpu, &conexion_entrante);
             pthread_detach(hilo_cpu);
