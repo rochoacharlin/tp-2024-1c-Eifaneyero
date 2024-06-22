51722e3da21065647e3cf2c7f2a93ab20a75fda6
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-06-22 23:59:03 +0000

    Memoria: vuelvo a colocar free que sí era necesario y no era problemático

--- memoria/src/gestion_memoria.c
+++ memoria/src/gestion_memoria.c
@@ -59,6 +59,7 @@ void atender_lectura_espacio_usuario(int sockete)
     recibir_lectura_espacio_usuario(sockete, &PID, &direccion_fisica, &tamanio_a_leer);
     void *valor_leido = leer_espacio_usuario(direccion_fisica, tamanio_a_leer);
     enviar_valor_leido(sockete, valor_leido, tamanio_a_leer);
+    free(valor_leido);
     loggear_lectura_espacio_de_usuario(PID, direccion_fisica, tamanio_a_leer);
 }
 
