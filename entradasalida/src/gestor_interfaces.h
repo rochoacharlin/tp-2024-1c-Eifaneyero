6fbe2b303c4ca028d861b943493f4ddc11dca8ea
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-07-09 18:55:48 +0000

    E/S: Modifico nombre de prototipo

--- entradasalida/src/gestor_interfaces.h
+++ entradasalida/src/gestor_interfaces.h
@@ -23,7 +23,7 @@ op_code atender_dialfs(int cod_op, t_list *parametros);
 
 bool escribir_en_memoria(uint32_t PID, t_list *direcciones_fisicas, void *escritura);
 void enviar_lectura_espacio_usuario(uint32_t PID, uint32_t direccion, uint32_t bytes_a_leer);
-op_code leer_de_memoria(uint32_t PID, uint32_t cantidad_bytes, t_list *direcciones_fisicas, char *valor_leido_completo);
+op_code leer_de_memoria(uint32_t PID, uint32_t cantidad_caracteres, t_list *direcciones_fisicas, char *valor_leido_completo);
 void recibir_lectura_parcial_memoria(char *valor_leido_completo, int desplazamiento, uint32_t bytes_a_operar);
 void setear_config(char *archivo_config_io);
 
