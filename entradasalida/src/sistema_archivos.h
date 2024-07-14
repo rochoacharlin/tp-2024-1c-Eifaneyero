e6a1d7c8c95dd8dcca38a3f0cc52faeb73aecb1d
Facundo Gauna Soma <fgaunasoma@frba.utn.edu.ar>
2024-07-14 05:49:25 +0000

    E/S: testeo - agrego leerBitmap, asignarBloque y desasignarBloque

--- entradasalida/src/sistema_archivos.h
+++ entradasalida/src/sistema_archivos.h
@@ -50,5 +50,8 @@ void mover_fcb(t_fcb *fcb, int nuevo_inicio);
 void mover_contenido_fcb(t_fcb *fcb, int nuevo_inicio, void *src_contenido, bool hacer_clean, int tamanio_a_truncar);
 void compactar(uint32_t *PID, t_fcb *archivo_a_truncar, int tamanio_execedente_en_bloques);
 int bytes_a_bloques(int bytes);
+char *leer_bitmap(const t_bitarray *bitmap, size_t indice_inico, size_t cantidad_de_bits);
+void assignBlock(int blockIndex);
+void unassignBlock(int blockIndex);
 
 #endif
