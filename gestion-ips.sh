4520fe2423a7392a813ac78e7eaf13748c5bbfa5
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-07-18 19:11:27 +0000

    Script Shell: Dejo de tener en cuenta los puertos

--- gestion-ips.sh
+++ gestion-ips.sh
@@ -8,34 +8,17 @@ modulos=("MEMORIA" "CPU" "KERNEL")
 
 # Inicializar arrays para IPs y Puertos
 declare -A IPs
-declare -A Puertos
 
 # Capturar IPs y Puertos del usuario
 for modulo in "${modulos[@]}"; do
     echo "Ingresar IP de la ${modulo}: "
     read IPs["${modulo}"]
-
-    if [ "$modulo" == "CPU" ]; then
-        echo "Ingresar Puerto INTERRUPT de la ${modulo}: "
-        read Puertos["${modulo}_INTERRUPT"]
-
-        echo "Ingresar Puerto DISPATCH de la ${modulo}: "
-        read Puertos["${modulo}_DISPATCH"]
-    else
-        echo "Ingresar Puerto de la ${modulo}: "
-        read Puertos["${modulo}"]
-    fi
 done
 
 # Reemplazar IPs y Puertos en los archivos .config
 for archivo in "${archivos[@]}"; do
     for modulo in "${modulos[@]}"; do
         sed -i "s/IP_${modulo}=[a-z0-9\.]*/IP_${modulo}=${IPs[${modulo}]}/" "$archivo"
-        if [ "$modulo" == "CPU" ]; then
-            sed -i "s/PUERTO_${modulo}_INTERRUPT=[a-z0-9\.]*/PUERTO_${modulo}_INTERRUPT=${Puertos[${modulo}_INTERRUPT]}/" "$archivo"
-            sed -i "s/PUERTO_${modulo}_DISPATCH=[a-z0-9\.]*/PUERTO_${modulo}_DISPATCH=${Puertos[${modulo}_DISPATCH]}/" "$archivo"
-        else
-            sed -i "s/PUERTO_${modulo}=[a-z0-9\.]*/PUERTO_${modulo}=${Puertos[${modulo}]}/" "$archivo"
-        fi
     done
 done
+
